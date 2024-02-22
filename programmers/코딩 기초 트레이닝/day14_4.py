6b58a10cfe56dfc47f1653ba0a11f3b2ce419cf4
dyoon-23 <150774513+dyoon-23@users.noreply.github.com>
2024-02-22 22:39:34 +0900

    리스트(배열)
    홀수 vs 짝수
    5명씩
    할 일 목록
    n보다 커질 때까지 더하기
    수열과 구간 쿼리 1

---
+++ programmers/코딩 기초 트레이닝/day14_4.py
@@ -0,0 +1,14 @@
+# 정수 배열 numbers와 정수 n이 매개변수로 주어집니다.
+# numbers의 원소를 앞에서부터 하나씩 더하다가 그 합이 n보다 커지는 순간 이때까지 더했던 원소들의 합을 return 하는 solution 함수를 작성해 주세요.
+
+def solution(numbers, n):
+    answer = 0
+    for i in range(len(numbers)):    #
+        if answer <= n:
+            answer += numbers[i]     #
+    return answer
+
+# i=0
+#     while answer<=n:        while 쓰기!
+#         answer+=numbers[i]
+#         i+=1
